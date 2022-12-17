7865ec2a28f70a12c415f433a82fb993995bca2c
Charlie Salgado <csalga20@depaul.edu>
2022-12-17 15:44:01 -0600

    Initial Commit

---
+++ realmpractice/realmpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  realmpracticeApp.swift
+//  realmpractice
+//
+//  Created by Charles Salgado on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct realmpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
